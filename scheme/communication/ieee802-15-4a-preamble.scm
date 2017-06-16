76a23e32b1533a8689f3f28ddc11733790ee5ca6
Frank Terbeck <ft@bewatermyfriend.org>
2017-06-17 00:22:44 +0200

    Add file header comment to ieee preamble module
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/communication/ieee802-15-4a-preamble.scm
+++ scheme/communication/ieee802-15-4a-preamble.scm
@@ -1,3 +1,25 @@
+;; Copyright © 2017 Frank Terbeck <ft@bewatermyfriend.org>
+;;
+;; This library  is free  software; you  can redistribute  it and/or  modify it
+;; under the terms of the GNU Lesser General Public License as published by the
+;; Free  Software Foundation;  either version  3 of  the License,  or (at  your
+;; option) any later version.
+;;
+;; This library is distributed in the hope  that it will be useful, but WITHOUT
+;; ANY  WARRANTY;  without even  the  implied  warranty of  MERCHANTABILITY  or
+;; FITNESS FOR A PARTICULAR PURPOSE. See  the GNU Lesser General Public License
+;; for more details.
+;;
+;; You should  have received a  copy of the  GNU Lesser General  Public License
+;; along with  this library;  if not,  write to  the Free  Software Foundation,
+;; Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+;;
+;; Commentary:
+;;
+;; This module  implements ternary  sequences as  used in  ieee-802.15.4a ultra
+;; wideband  beacon  preambles.  The  module uses  the  linear  feedback  shift
+;; register module (communication lfsr) with LFSRs in Galois configuration.
+
 (define-module (communication ieee802-15-4a-preamble)
   #:use-module (srfi srfi-41)
   #:use-module (communication lfsr)
